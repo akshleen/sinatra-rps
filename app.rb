c664b5208606e45323f8c45c5eb50c660e1639a7
Akshleen Dhanoa <161362298+akshleen@users.noreply.github.com>
2025-02-03 05:13:06 +0000

    add erb views and logic for rps game

--- app.rb
+++ app.rb
@@ -1,9 +1,44 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Homepage
+get "/" do
+  erb :index
+end
+
+# Rock Route
+get "/rock" do
+  @player_choice = "rock"
+  play_game(@player_choice)
+  erb :result
+end
+
+# Paper Route
+get "/paper" do
+  @player_choice = "paper"
+  play_game(@player_choice)
+  erb :result
+end
+
+# Scissors Route
+get "/scissors" do
+  @player_choice = "scissors"
+  play_game(@player_choice)
+  erb :result
+end
+
+# Helper Method to Play the Game
+def play_game(player_choice)
+  choices = ["rock", "paper", "scissors"]
+  @computer_choice = choices.sample
+
+  if player_choice == @computer_choice
+    @result_message = "tied"
+  elsif (player_choice == "rock" && @computer_choice == "scissors") ||
+        (player_choice == "paper" && @computer_choice == "rock") ||
+        (player_choice == "scissors" && @computer_choice == "paper")
+    @result_message = "won"
+  else
+    @result_message = "lost"
+  end
 end
